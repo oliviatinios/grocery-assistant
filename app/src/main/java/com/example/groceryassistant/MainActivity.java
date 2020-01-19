52c06b457f3cce58b0123cde4533e6d3350ed89a
oliviatinios <oliviatinios@gmail.com>
2020-01-19 17:19:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/groceryassistant/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.groceryassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Establish connection to AWS
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
+
+        setContentView(R.layout.activity_main);
+    }
+}
