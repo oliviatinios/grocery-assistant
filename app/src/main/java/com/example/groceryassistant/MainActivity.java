a8cbed83699bdf4178e91459f2ed56a41c5464da
Will Conry <will@conry.ca>
2020-03-25 12:54:56 -0400

    Initial commit, crashes when attempting speech

--- app/src/main/java/com/example/groceryassistant/MainActivity.java
+++ app/src/main/java/com/example/groceryassistant/MainActivity.java
@@ -18,6 +18,8 @@ import android.graphics.Path;
 import android.graphics.PointF;
 import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -34,6 +36,7 @@ import android.widget.Toast;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.AWSStartupHandler;
 import com.amazonaws.mobile.client.AWSStartupResult;
@@ -41,7 +44,14 @@ import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
 import com.amazonaws.mobileconnectors.lex.interactionkit.Response;
 import com.amazonaws.mobileconnectors.lex.interactionkit.config.InteractionConfig;
 import com.amazonaws.mobileconnectors.lex.interactionkit.ui.InteractiveVoiceView;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.polly.AmazonPollyPresigningClient;
+import com.amazonaws.services.polly.model.DescribeVoicesRequest;
+import com.amazonaws.services.polly.model.DescribeVoicesResult;
+import com.amazonaws.services.polly.model.OutputFormat;
+import com.amazonaws.services.polly.model.SynthesizeSpeechPresignRequest;
+import com.amazonaws.services.polly.model.Voice;
 import com.navigine.naviginesdk.DeviceInfo;
 import com.navigine.naviginesdk.Location;
 import com.navigine.naviginesdk.LocationPoint;
@@ -54,7 +64,10 @@ import com.navigine.naviginesdk.Venue;
 import com.navigine.naviginesdk.Zone;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -230,7 +243,9 @@ public class MainActivity extends Activity {
 
     public void onZoomIn(View v)
     {
+        //talk.speak("I am Amazon voice");
         gui.zoomLocationView(1.25f);
+        talktalk("Hello world!");
     }
 
     public void onZoomOut(View v)
@@ -1098,4 +1113,77 @@ public class MainActivity extends Activity {
                 .setAwsRegion(getApplicationContext()
                         .getString(R.string.aws_region));
     }
+
+    void talktalk(String text) {
+        // Cognito pool ID. Pool needs to be unauthenticated pool with
+// Amazon Polly permissions.
+        String COGNITO_POOL_ID = "us-east-1:02e43abc-0cb2-4c63-80c1-11b22e36364b";
+
+// Region of Amazon Polly.
+        Regions MY_REGION = Regions.US_EAST_1;
+
+// Initialize the Amazon Cognito credentials provider.
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                COGNITO_POOL_ID,
+                MY_REGION
+        );
+
+// Create a client that supports generation of presigned URLs.
+        AmazonPollyPresigningClient client = new AmazonPollyPresigningClient(credentialsProvider);
+
+        // Create describe voices request.
+        DescribeVoicesRequest describeVoicesRequest = new DescribeVoicesRequest();
+
+// Synchronously ask Amazon Polly to describe available TTS voices.
+        DescribeVoicesResult describeVoicesResult = client.describeVoices(describeVoicesRequest);
+        List<Voice> voices = describeVoicesResult.getVoices();
+
+        // Create speech synthesis request.
+        SynthesizeSpeechPresignRequest synthesizeSpeechPresignRequest =
+                new SynthesizeSpeechPresignRequest()
+                        // Set the text to synthesize.
+                        .withText(text)
+                        // Select voice for synthesis.
+                        .withVoiceId(voices.get(0).getId()) // "Joanna"
+                        // Set format to MP3.
+                        .withOutputFormat(OutputFormat.Mp3);
+
+// Get the presigned URL for synthesized speech audio stream.
+        URL presignedSynthesizeSpeechUrl =
+                client.getPresignedSynthesizeSpeechUrl(synthesizeSpeechPresignRequest);
+
+        // Use MediaPlayer: https://developer.android.com/guide/topics/media/mediaplayer.html
+
+// Create a media player to play the synthesized audio stream.
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            // Set media player's data source to previously obtained URL.
+            mediaPlayer.setDataSource(presignedSynthesizeSpeechUrl.toString());
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to set data source for the media player! " + e.getMessage());
+        }
+
+// Prepare the MediaPlayer asynchronously (since the data source is a network stream).
+        mediaPlayer.prepareAsync();
+
+// Set the callback to start the MediaPlayer when it's prepared.
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+// Set the callback to release the MediaPlayer after playback is completed.
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+    }
+
 }
